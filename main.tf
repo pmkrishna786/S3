a0d9b7d9922ff6c06177bdd4556a8855e1534c9c
pmkrishna786 <80220383+pmkrishna786@users.noreply.github.com>
2023-07-15 23:34:30 +0530

    Updating S3 access to Public

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 resource "aws_s3_bucket" "bucket" {
   bucket = "muraliJenkins"
-  acl    = "private"
+  acl    = "public"
 
   tags = {
     #Name        = "cehtestingfromterraform"
