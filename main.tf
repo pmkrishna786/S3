916cb62842389bc58c2f151629f9981ba1756967
pmkrishna786 <80220383+pmkrishna786@users.noreply.github.com>
2023-07-15 22:37:19 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "AKIA3DSBZQFHJJSSOHYD"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+resource "aws_s3_bucket" "bucket" {
+  bucket = "cehtestingfromterraform"
+  acl    = "private"
+
+  tags = {
+    #Name        = "cehtestingfromterraform"
+    Environment = "test"
+  }
+}
